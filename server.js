b62062abc4fa1e9c284694b2ee8ec1244d5c266b
Trey Briggs <tebriggs86@gmail.com>
2014-10-21 21:34:55 -0500

    Fixing up links

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var server = express();
 server.use(express.static(__dirname + '/'));
  
-var port = 10001;
+var port = 10002;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
